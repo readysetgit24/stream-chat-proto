4f3c3ae03f3c065fc0aac7e89b2052a944afbec9
afolga <85518452+afolga@users.noreply.github.com>
2022-11-20 21:23:02 -0600

    trying to get chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ChatClientService, ChannelService, StreamI18nService, MessageContext, CustomTemplatesService, ChannelPreviewContext } from 'stream-chat-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'first-angular-app';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('customMessageTemplate') messageTemplate!: TemplateRef<MessageContext>;
+  @ViewChild('customChannelPreviewTemplate') channelPreviewTemplate!: TemplateRef<ChannelPreviewContext>;
+
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService,
+    private customTemplatesService: CustomTemplatesService
+  ) {
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'square-thunder-7';
+    const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoic3F1YXJlLXRodW5kZXItNyIsImV4cCI6MTY2ODk4MjEwOX0.VFyrmFxYU4CJzI0MY4kgjJKidOnDrJovglqZfTE09Co';
+    this.chatService.init(apiKey, userId, userToken);
+    this.streamI18nService.setTranslation();
+  }
+
+  async ngOnInit() {
+    const channel = this.chatService.chatClient.channel('messaging', 'talking-about-angular', {
+      // add as many custom fields as you'd like
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      name: 'Talking about Angular',
+    });
+    await channel.create();
+    this.channelService.init({
+      type: 'messaging',
+      id: { $eq: 'talking-about-angular' },
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.customTemplatesService.messageTemplate$.next(this.messageTemplate);
+    this.customTemplatesService.channelPreviewTemplate$.next(this.channelPreviewTemplate);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StreamChatModule, StreamAutocompleteTextareaModule } from 'stream-chat-angular';
+import { MessageComponent } from './message/message.component';
+import { ChannelPreviewComponent } from './channel-preview/channel-preview.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MessageComponent, ChannelPreviewComponent],
+  imports: [BrowserModule, TranslateModule.forRoot(), StreamAutocompleteTextareaModule, StreamChatModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
